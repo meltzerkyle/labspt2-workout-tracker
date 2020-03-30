9683070cb756f8c81d1d6ef89dcc3c7431f79f1c
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-03-30 15:46:36 -0600

    React Big Calendar replaces React Calendar, to account for event functionality

--- front-end/src/components/TCalendar.js
+++ front-end/src/components/TCalendar.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -7,145 +6,64 @@ import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClic
 import '@fullcalendar/core/main.css';
 import { clickedDate, updateEvent } from '../actions/actions.js';
 import listWeek from '@fullcalendar/list';
-import moment from 'moment';
-import Calendar from 'react-calendar';
 
 import 'react-calendar/dist/Calendar.css';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import './styles/Calendar.scss';
 
-class TCalendar extends React.Component {
-  calendarComponentRef = React.createRef();
+const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
 
+class TCalendar extends Component {
   state = {
-    calendarWeekends: true,
-    eventClicked: false,
-    calendarEvents: [
-      // initial event data
+    events: [
       {
-        id: 1,
-        title: 'Arms',
-        start: '2019-04-12T13:30:00',
-        end: '2019-04-12T14:30:00',
-        allDay: false
-      },
-      {
-        id: 2,
-        title: 'Legs',
-        start: '2019-04-12T15:30:00',
-        end: '2019-04-12T16:30:00',
-        allDay: false
-      },
-      {
-        id: 3,
-        title: 'Core',
-        start: '2019-04-12T20:30:00',
-        end: '2019-04-12T21:30:00',
-        allDay: false
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
+        title: "Some title"
       }
     ]
   };
 
-  handleEventClick = info => {
-    this.setState({ eventClicked: true });
-    console.log(this.state.eventClicked);
-    console.log(info.event.start.toISOString());
-    const date = info.event.start.toISOString();
-    this.props.clickedDate(date);
-  };
-
-  handleDateClick = ({ date }) => {
-    let time = '11:36 pm';
-    console.log('this is the date:');
-    console.log(date.toISOString().substring(0, 10));
-    if (date.toISOString() === this.props.dateClicked) {
-      this.props.clickedDate('');
-    } else {
-      this.props.clickedDate(date.toISOString());
-    }
-  };
-
-  handleSelect = selectionInfo => {
-    // console.log(selectionInfo);
-    console.log('this is the start:');
-    console.log(selectionInfo.start.toISOString());
-    console.log('this is the end:');
-    console.log(selectionInfo.end.toISOString());
-  };
-
-  handleDragAndDrop = obj => {
-    let singleEvent = this.props.events.filter(event => {
-      return event.id.toString() === obj.event.id.toString();
-    })[0];
-
-    let exceptOne = this.props.events.filter(event => {
-      return event.id.toString() !== obj.event.id.toString();
+  onEventResize = (type, { event, start, end, allDay }) => {
+    this.setState(state => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: state.events };
     });
-    this.props.updateEvent(exceptOne);
+  };
 
-    singleEvent.start = moment(obj.event.start)
-      .format()
-      .substring(0, 19);
-    singleEvent.end = moment(obj.event.end)
-      .format()
-      .substring(0, 19);
-
-    let updatedEvents = [singleEvent, ...exceptOne];
-
-    this.props.updateEvent(updatedEvents);
+  onEventDrop = ({ event, start, end, allDay }) => {
+    console.log(start);
   };
 
   render() {
-    const events = this.props.events !== undefined && this.props.events;
     return (
-      <div className='schedule-calendar'>
-
-        <div className='calendar'>
-          <Calendar />
-          {/* <FullCalendar
-            defaultView='dayGridMonth'
-            header={{
-              left: 'prevYear, prev,next, nextYear today',
-              center: 'title',
-              right: ''
-            }}
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            ref={this.calendarComponentRef}
-            fixedWeekCount={false}
-            weekends={this.state.calendarWeekends}
-            events={events}
-            dayPopoverFormat={{
-              month: 'short',
-              day: 'numeric'
-            }}
-            displayEventTime={false}
-            dateClick={this.handleDateClick}
-            eventClick={this.handleEventClick}
-            selectable={true}
-            select={this.handleSelect}
-            editable={true}
-            eventLimit={true} // for all non-TimeGrid views
-            eventLimit={2}
-            eventDrop={this.handleDragAndDrop}
-            aspectRatio={1}
-            height={750}
-          /> */}
-        </div>
+      <div className="calendar">
+        <DnDCalendar
+          defaultDate={moment().toDate()}
+          defaultView="month"
+          events={this.state.events}
+          localizer={localizer}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+        />
       </div>
     );
   }
-
-  toggleWeekends = () => {
-    this.setState({
-      // update a property
-      calendarWeekends: !this.state.calendarWeekends
-    });
-  };
-
-  gotoPast = () => {
-    let calendarApi = this.calendarComponentRef.current.getApi();
-    calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
-  };
 }
+
 
 const mapStateToProps = state => {
   return {
