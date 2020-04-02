59ebf605c81fc40a63ff2e783ebe965bb06c39de
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-04-02 17:52:25 -0600

    fixed time issue with events

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3333;
 
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
